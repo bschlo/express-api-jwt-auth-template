321d6635b36b6f08df566f8a924494d11e33e995
bschloss <benschloss2@gmail.com>
2024-12-13 09:48:35 -0500

    Adding index route.

--- server.js
+++ server.js
@@ -24,6 +24,10 @@ app.use('/test-jwt', testJWTRouter)
 app.use('/users', usersRouter)
 app.use('/profiles', profilesRouter)
 
+app.get('/', (req, res) => {
+    res.json({"message": 'Hello'})
+})
+
 
 app.listen(PORT, () => {
   console.log(`The express app is ready on ${PORT}!`);
